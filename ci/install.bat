b7f81f06a2543540e3728481d6a52b768d6970a6
Spencer Young <spencer.young@spyoung.com>
2019-01-25 22:09:32 -0800

    add wheel to build requirements

--- ci/install.bat
+++ ci/install.bat
@@ -1,5 +1,6 @@
 py -3.7 -m venv venv
 call venv\Scripts\activate.bat
 python -m pip install --upgrade pip
-python -m pip install --upgrade .
+python -m pip install --upgrade wheel
+python -m pip install .
 call deactivate
