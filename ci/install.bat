70cfae9b3d92ecf4572024e04c521316533bc22e
Spencer Young <spencer.young@spyoung.com>
2019-01-25 21:33:43 -0800

    slim install

--- ci/install.bat
+++ ci/install.bat
@@ -1,8 +1,5 @@
 py -3.7 -m venv venv
 call venv\Scripts\activate.bat
 python -m pip install --upgrade pip
-python -m pip install --upgrade setuptools
 python -m pip install --upgrade .
-python -m pip install --upgrade wheel
-python -m pip install --upgrade twine
 call deactivate
